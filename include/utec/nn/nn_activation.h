8f0c9a709ee720aaf330425d7b7d0b3d877bb3f6
dfigueroaw <dfigueroaw@gmail.com>
2025-07-04 21:31:32 -0500

    feat: add tanh function

--- include/utec/nn/nn_activation.h
+++ include/utec/nn/nn_activation.h
@@ -38,6 +38,19 @@ namespace utec::neural_network {
         }
     };
 
+    template<typename T>
+        class Tanh final : public ILayer<T> {
+        Tensor<T, 2> output{0, 0};
+    public:
+        Tensor<T,2> forward(const Tensor<T,2>& z) override {
+            output = algebra::apply(z, [](const T t) { return std::tanh(t); });
+            return output;
+        }
+
+        Tensor<T,2> backward(const Tensor<T,2>& g) override {
+            return algebra::apply(output, [](const T t ) { return 1 - t * t; }) * g;
+        }
+    };
 }
 
 #endif //PROG3_NN_FINAL_PROJECT_V2025_01_ACTIVATION_H
