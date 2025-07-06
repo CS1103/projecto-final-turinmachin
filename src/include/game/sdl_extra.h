1a8775f1bce33ca64f3f09a912478d966e420dec
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-06 00:54:45 -0500

    refactor: remove an unused function

--- src/include/game/sdl_extra.h
+++ src/include/game/sdl_extra.h
@@ -2,17 +2,10 @@
 #define INCLUDE_GAME_SDL_EXTRA_H
 
 #include <SDL_render.h>
-#include <cstdint>
+
 namespace sdl_extra {
 
     auto get_performance_time() -> long double;
-
-    void draw_better_thick_line(SDL_Renderer* renderer,
-                                float x1,
-                                float y1,
-                                float x2,
-                                float y2,
-                                int thickness);
 
 }  // namespace sdl_extra
 
