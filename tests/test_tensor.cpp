5c59a60fec067ed8489814bd749961dda77ca854
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-09 21:19:07 -0500

    build: bundle catch in the repo

--- tests/test_tensor.cpp
+++ tests/test_tensor.cpp
@@ -1,6 +1,6 @@
 #include <utec/algebra/tensor.h>
 #include <array>
-#include <catch2/catch_test_macros.hpp>
+#include <catch/catch_amalgamated.hpp>
 #include <stdexcept>
 
 using utec::algebra::Tensor;
