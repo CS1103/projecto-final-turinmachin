3b8f982cc2b49da33c7903e3e0de7965ad2ea6f1
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-09 21:49:28 -0500

    build: include vendored SDL_ttf

--- shell.nix
+++ shell.nix
@@ -2,17 +2,6 @@
   pkgs ? import <nixpkgs> { },
   ...
 }:
-let
-  inherit (pkgs)
-    mkShell
-    sdl3
-    sdl3-ttf
-    ;
-in
-mkShell {
+pkgs.mkShell {
   inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
-
-  env = {
-    CMAKE_PREFIX_PATH = "${sdl3.dev}/lib/cmake:${sdl3-ttf}/lib/cmake";
-  };
 }
