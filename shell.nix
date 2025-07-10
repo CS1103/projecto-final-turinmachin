041703f1c7a4126dcd7558d00018e1d086e09dae
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-09 22:55:47 -0500

    build: always use vendored catch

--- shell.nix
+++ shell.nix
@@ -7,13 +7,12 @@ let
     mkShell
     sdl3
     sdl3-ttf
-    catch2_3
     ;
 in
 mkShell {
   inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
 
   env = {
-    CMAKE_PREFIX_PATH = "${sdl3.dev}/lib/cmake:${sdl3-ttf}/lib/cmake:${catch2_3}/lib/cmake";
+    CMAKE_PREFIX_PATH = "${sdl3.dev}/lib/cmake:${sdl3-ttf}/lib/cmake";
   };
 }
